ba594789b9ecc39bcb7c3ce1687357e2f1b8cce9
Huy Nguyen <huynn109@gmail.com>
2018-06-04 18:06:51 +0700

    update backdrop

---
+++ ex_04_backdrop/lib/category.dart
@@ -0,0 +1,20 @@
+import 'package:ex_04_backdrop/unit.dart';
+import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
+
+class Category {
+  final String name;
+  final ColorSwatch color;
+  final List<Unit> units;
+  final IconData iconLocation;
+
+  const Category(
+      {@required this.name,
+      @required this.color,
+      @required this.units,
+      @required this.iconLocation})
+      : assert(name != null),
+        assert(color != null),
+        assert(units != null),
+        assert(iconLocation != null);
+}
---
+++ ex_04_backdrop/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'category_route.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Unit Converter',
+      theme: ThemeData(
+        textTheme: Theme.of(context).textTheme.apply(
+          bodyColor: Colors.black,
+          displayColor: Colors.grey[600],
+        ),
+        // This colors the [InputOutlineBorder] when it is selected
+        primaryColor: Colors.grey[500],
+        textSelectionHandleColor: Colors.green[500],
+      ),
+      home: CategoryRoute(),
+    );
+  }
+}
+
