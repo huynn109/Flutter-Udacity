e169034b5704f3b4ae1611e3020e3ca8264455af
huynn109 <huynn109@gmail.com>
2018-06-05 00:29:04 +0700

    update route

--- ex_04_backdrop/lib/category_route.dart
+++ ex_04_backdrop/lib/category_route.dart
@@ -8,7 +8,8 @@ import 'unit_converter.dart';
 class CategoryRoute extends StatefulWidget {
   const CategoryRoute();
 
-  @override _CategoryRouteState createState() => _CategoryRouteState();
+  @override
+  _CategoryRouteState createState() => _CategoryRouteState();
 }
 
 final _backgroundColor = Colors.green[100];
@@ -65,12 +66,11 @@ class _CategoryRouteState extends State<CategoryRoute> {
     }),
   ];
 
-
   @override
   void initState() {
     super.initState();
     for (int i = 0; i < _categoryNames.length; i++) {
-     var category = Category(
+      var category = Category(
         name: _categoryNames[i],
         color: _baseColors[i],
         iconLocation: Icons.cake,
@@ -94,33 +94,46 @@ class _CategoryRouteState extends State<CategoryRoute> {
   /// Makes the correct number of rows for the list view.
   ///
   /// For portrait, we use a [ListView].
-  Widget _buildCategoryWidgets() {
-    return ListView.builder(
-      itemBuilder: (BuildContext context, int index) {
-        return CategoryTile(
-          category: _categories[index],
-          onTap: _onCategoryTap,
-        );
-      },
-      itemCount: _categories.length,
-    );
+  Widget _buildCategoryWidgets(Orientation orientation) {
+    if (orientation == Orientation.portrait) {
+      return ListView.builder(
+        itemBuilder: (BuildContext context, int index) {
+          return CategoryTile(
+            category: _categories[index],
+            onTap: _onCategoryTap,
+          );
+        },
+        itemCount: _categories.length,
+      );
+    } else {
+      return GridView.count(
+        crossAxisCount: 2,
+        childAspectRatio: 3.0,
+        children: _categories.map((Category c) {
+          return CategoryTile(
+            category: c,
+            onTap: _onCategoryTap,
+          );
+        }).toList(),
+      );
+    }
   }
-
 
   @override
   Widget build(BuildContext context) {
+    assert(debugCheckHasMediaQuery(context));
     final listView = Padding(
       padding: EdgeInsets.only(
         left: 8.0,
         right: 8.0,
         bottom: 48.0,
       ),
-      child: _buildCategoryWidgets(),
+      child: _buildCategoryWidgets(MediaQuery.of(context).orientation),
     );
 
     return Backdrop(
       currentCategory:
-      _currentCategory == null ? _defaultCategory : _currentCategory,
+          _currentCategory == null ? _defaultCategory : _currentCategory,
       frontPanel: _currentCategory == null
           ? UnitConverter(category: _defaultCategory)
           : UnitConverter(category: _currentCategory),
@@ -140,5 +153,4 @@ class _CategoryRouteState extends State<CategoryRoute> {
       );
     });
   }
-
 }
